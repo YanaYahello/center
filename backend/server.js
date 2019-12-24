ab8eeedaa56a38bd9daf2686b5a5d4de1cc33474
Yana Yahello <yana.yahello@inventorsoft.co>
2019-12-24 15:58:59 +0200

    backend data

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,23 @@
 const express    = require('express'),
       nodeMailer = require('nodemailer'),
-      cors = require('cors'),
+      cors       = require('cors'),
       bodyParser = require('body-parser');
 
 const app = express();
-app.use(cors({origin: "*"}));
+app.use(cors({origin: '*'}));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 const port = 3000;
-app.listen(port, function(){
+app.listen(port, function () {
     console.log('Server is running at port: ', port);
 });
-app.get('/',  (req, res) => {
+app.get('/', (req, res) => {
     res.render('index');
 });
+
 app.post('/send-email', (req, res) => {
     const userData = req.body;
     sendmail(userData, info => {
@@ -25,22 +26,28 @@ app.post('/send-email', (req, res) => {
     });
 });
 
-async function sendmail(user, callback){
+async function sendmail(user, callback) {
     let transporter = nodeMailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false, //true for 465 , false for other port
-        auth: {
-            user: 'yahellonchik@gmail.com',
-            pass:''
+        service: 'Gmail',
+        auth   : {
+            user: 'yana.yahello@inventorsoft.co',
+            pass: 'uyckrtzkqjsyansm'
+        },
+        tls    : {
+            rejectUnauthorized: false
         }
     });
+
     let mailOptions = {
-        from: `first-name: ${user.firstName}, last-name: ${user.lastName}, email: ${user.email}`,
-        to: 'yahellonchik@gmail.com',
+        from   : `email: ${user.email}`,
+        to     : `${user.email}`,
         subject: 'letter form client',
         text: user.question,
-        html: '<b>CENTER</b>'
+        html   : '<b>CENTER</b>' +
+            `<p>Имя: ${user.firstName}</p>
+             <p>Фамилия: ${user.lastName}</p>
+             <p>Email: ${user.email}</p>
+             <p>Question: ${user.question}</p>`
     };
 
     let info = await transporter.sendMail(mailOptions);
