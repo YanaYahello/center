48183efcaafad9caa9c2d25d39eaa43fab178249
Yana Yahello <yana.yahello@inventorsoft.co>
2019-12-24 17:19:08 +0200

    updated project

--- ui/src/app/shared/components/contact-us-form/contact-us-form.component.ts
+++ ui/src/app/shared/components/contact-us-form/contact-us-form.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+<<<<<<< HEAD
 import { EmailService } from '../../../common/services/email.service';
+=======
+>>>>>>> created form added google cart
 
 @Component({
     selector   : 'app-contact-us-form',
@@ -10,8 +13,12 @@ import { EmailService } from '../../../common/services/email.service';
 export class ContactUsFormComponent implements OnInit {
     contactUsForm: FormGroup;
 
+<<<<<<< HEAD
     constructor(private formBuilder: FormBuilder,
                 private emailService: EmailService) { }
+=======
+    constructor(private formBuilder: FormBuilder) { }
+>>>>>>> created form added google cart
 
     ngOnInit() {
         this.createForm();
@@ -19,16 +26,26 @@ export class ContactUsFormComponent implements OnInit {
 
     createForm() {
         this.contactUsForm = this.formBuilder.group({
+<<<<<<< HEAD
             firstName: ['', [Validators.required, Validators.maxLength(15), Validators.minLength(2)]],
             lastName : ['', [Validators.required, Validators.maxLength(15), Validators.minLength(2)]],
             email    : ['', [Validators.required, Validators.email, Validators.maxLength(15), Validators.minLength(2)]],
             question : ['', [Validators.required, Validators.maxLength(200)]]
+=======
+            firstName: ['', [Validators.required, Validators.maxLength(30), Validators.minLength(2)]],
+            lastName : ['', [Validators.required, Validators.maxLength(30), Validators.minLength(2)]],
+            email    : ['', [Validators.required, Validators.maxLength(30), Validators.minLength(2)]],
+            question : ['', [Validators.required,  Validators.maxLength(200)]]
+>>>>>>> created form added google cart
         });
     }
 
     onSubmit() {
         console.log(this.contactUsForm.value);
+<<<<<<< HEAD
         this.emailService.sendMessage(this.contactUsForm.value);
         this.contactUsForm.reset();
+=======
+>>>>>>> created form added google cart
     }
 }
